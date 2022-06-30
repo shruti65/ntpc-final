7a720e9e748b9abd992ddfbd1982955f3677c2ca
shruti65 <91328611+shruti65@users.noreply.github.com>
2022-06-30 12:53:41 +0530

    Add files via upload

---
+++ item/config.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'material';
+$databaseUsername = 'root';
+$databasePassword = '';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, 
+$databaseName);  
+?>
